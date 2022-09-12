9645d7d5ab9b8ee133f30d70c9e314f527571280
AgustinTrigo(re) <a_trigo295@hotmail.com>
2022-09-12 14:50:32 -0300

    Add ItemDetailContainer + ItemDetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -3,15 +3,19 @@ import ItemCount from "./ItemCount";
 
 const ItemDetail = ({item}) =>{
     return(
-        <div>
-            {item.map(e => (
-                <div key={e.id} className="card__detail">
-                    <h2>{e.nombre}</h2>
-                    <p>{e.descripcion}</p>
-                    <h3>precio: $ {e.precio}</h3>
-                    <ItemCount var1={e.stock} var2={1}/>
-                </div>)
-            )}
+        <div className="card__detail">
+            <div className="card__detail--container">
+                <div className="card__detail--img">
+                    <h2>{item.nombre}</h2>
+                </div>
+                <div className="card__detail--text">
+                    <p>{item.descripcion}</p>
+                </div>
+                <div className="card__detail--inputs">
+                    <h3>precio: $ {item.precio}</h3>
+                    <ItemCount var1={item.stock} var2={1}/>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -5,17 +5,25 @@ import ItemDetail from "./ItemDetail";
 
 const ItemDetailContainer = () =>{
     
-    const [item, setItem] = useState([])
+    const [item, setItem] = useState({})
     useEffect(()=>{
 
         const productos = [
-            {"id":1, "nombre":"producto 1", "precio":100, "descripcion":"Este es el producto uno", "stock":4}
+            {"id":1, "nombre":"producto 1", "precio":100, "descripcion":"Este es el producto uno)", "stock":4},
+            {"id":2, "nombre":"producto 2", "precio":200, "descripcion":"Este es el producto dos", "stock":3},
+            {"id":3, "nombre":"producto 3", "precio":300, "descripcion":"Este es el producto tres", "stock":5},
+            {"id":4, "nombre":"producto 4", "precio":400, "descripcion":"Este es el producto cuatro", "stock":2},
+            {"id":5, "nombre":"producto 5", "precio":400, "descripcion":"Este es el producto cinco", "stock":2},
+            {"id":6, "nombre":"producto 6", "precio":400, "descripcion":"Este es el producto seis", "stock":2},
+            {"id":7, "nombre":"producto 7", "precio":400, "descripcion":"Este es el producto siete", "stock":2},
+            {"id":8, "nombre":"producto 8", "precio":400, "descripcion":"Este es el producto ocho", "stock":2},
         ];
 
         const getItem = new Promise((resolve) => {
             setTimeout(()=> {
-                resolve(productos);
-            }, 2000);
+                const producto = productos.find((e)=> e.id === 4)
+                resolve(producto);
+            }, 300);
         })
 
         getItem.then((respuesta) => {
