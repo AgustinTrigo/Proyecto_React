683ca65e7377600951efb391f01e699c702e9462
AgustinTrigo <a_trigo295@hotmail.com>
2022-09-28 00:47:24 -0300

    Configurando Checkout-2

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../context/cartContext";
 
 const Checkout = () =>{
+
+    const {cart, sumarTotal} = useContext(CartContext);
+
+
     return (
         <div className="checkout">
             <form className="cart-bg checkout__form">
@@ -13,6 +19,27 @@ const Checkout = () =>{
                 <input type="button" className="checkout__btn" value="comprar"/>
             </form>
             <div className="cart-bg checkout__detail">
+                <div className="checkout__detail--info">
+                    <h4>TOTAL: $  {sumarTotal()}</h4>
+                </div>
+                {cart.map(e =>(
+                    <div key={e.id} className="cart__items--container">
+                        <div className="cart__items--img">
+                            <img src={e.img} alt={e.nombre}/>
+                        </div>
+                        <div className="cart__items--data">
+                            <div className="cart__items--data_nombre">
+                                <h4>{e.nombre}</h4>
+                            </div>
+                            <div className="cart__items--data_info">
+                                <h4>${e.precio}</h4>
+                                <h4>x{e.cantidad}</h4>
+                                <h4>$ {e.subtotal}</h4>
+                            </div>
+                        </div>
+                    </div>
+                    ))
+                }
             </div>
         </div>
     )
