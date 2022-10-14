e29c046bf9a71fed738cf3283b45630d58dac715
AgustinTrigo(re) <a_trigo295@hotmail.com>
2022-10-14 14:44:55 -0300

    agregando detalles-3

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -3,8 +3,15 @@ import React from "react";
 const Orders = () =>{
     return(
         <div className="container">
-            <div className="buscador"></div>
-            <div className="detalle"></div>
+            <div className="buscador">
+                <p>Ingresa los datos para ver tu compra</p>
+                <label htmlFor="nombre">NOMBRE</label>
+                <input name="nombre" type="text"/>
+                <label htmlFor="orderNbr">ORDEN #</label>
+                <input name="orderNbr" type="text"/>
+                <input type="button" className="card__btn" value="BUSCAR"/>
+            </div>
+            
         </div>
     )
 }
